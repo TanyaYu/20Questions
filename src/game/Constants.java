536daf92580e8c23ce3e7557bc26b72f69d9c7b3
Aashirya Kaushik <ak1107@wildcats.unh.edu>
2018-11-19 19:14:09 -0500

    Adding questions

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -14,4 +14,79 @@ public interface Constants {
 	public final static List<String> NO = asList("no", "n");
 	public final static List<String> SOMETIMES = asList("sometimes", "s");
 	public final static List<String> UNKNOWN = asList("unknown", "u");
+	
+	public final static String[] ANIMAL_QUESTIONS =  {
+			"Does it have four limbs?",
+			"Does it have a fur?",
+			"Does it have a tail?",
+			"Can it fly?",
+			"Is it a mammal?",
+			"Is it venomous?",
+			"Can it live in water?",
+			"Can it live on land?",
+			"Does it have gills?",
+			"Can it swim?",
+			"Is it a domestic animal?",
+			"Does it have a beak?",
+			"Does it have stripes?",
+			"Does it have scales?",
+			"Is it herbivorous?",
+			"Is it omnivorous?",
+			"Does it have feathers?",
+			"Does it have horn?",
+			"Can it run fast?"
+			
+			
+	};
+	
+	public final static String[] THINGS_QUESTIONS =  {
+			"Is it used for cleaning?",
+			"Does it come in different colors?",
+			"Does it have a particular smell?",
+			"Is it expensive?",
+			"Can you write with it?",
+			"Can you write on it?",
+			"Does it require electricity to run?",
+			"Can it be used for storage?",
+			"Is it commonly found in every household?",
+			"Can it be played with?",
+			"Can it be used for travelling?",
+			"Does it come in different sizes?",
+			"Does is come in various shapes?",
+			"Is it a necessisty?",
+			"Is it edible?",
+			"Is it a part of school stationery?",
+			"Is it breakable?",
+			"Does it run on batteries?",
+			"Is it used for educational purposes?",			
+	};
+	
+	public final static String[] STATE_QUESTIONS =  {
+			"Is the state in the south?",
+			"Is the state in the north?",
+			"Is the state in the east?",
+			"Is the state in the west?",
+			"Is the state heavily populated?",
+			"Is the state have a small population?",
+			"Is the state made up of two words?",
+			"Does it have desert?",
+			"Is the state part of the Appilachian or Rocky Mountains?",
+			"is the state mostly flat?",
+			"Does it border an Ocean?",
+			"Is it located near the center of the United States?",
+			"Is it part of the continental united states?",
+			"Does it snow there regularly?",
+			"Does it have an NFL Team located in the state?",
+			"Is it in the Eastern Time Zone?",
+			"Is it a popular tourist destination?",
+			"Is there alot of people that speak spanish there?",
+			"Does it get alot of rain?",
+			"Does it get Tornados?",
+			
+			
+			
+			
+	};
+	
+	
 }
