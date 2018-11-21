2267e2e62327fc4a3f4499149c9a5d5afdb63ef3
Aashirya Kaushik <ak1107@wildcats.unh.edu>
2018-11-21 16:43:25 -0500

    Adding Unknown and Sometimes

--- src/gamestates/AskQuestionState.java
+++ src/gamestates/AskQuestionState.java
@@ -59,6 +59,14 @@ public class AskQuestionState implements GameState {
 			database.postAnswer(questionNumber, Answer.NO);
 			if(questionNumber >= 20) return new GuessWordState();
 			else return new AskQuestionState(categoryId, questionNumber+1);
+		} else if(event instanceof OnSometimesEvent) {
+			database.postAnswer(questionNumber, Answer.SOMETIMES);
+			if(questionNumber >= 20) return new GuessWordState();
+			else return new AskQuestionState(categoryId, questionNumber+1);
+		} else if(event instanceof OnUnknownEvent) {
+			database.postAnswer(questionNumber, Answer.UNKNOWN);
+			if(questionNumber >= 20) return new GuessWordState();
+			else return new AskQuestionState(categoryId, questionNumber+1);
 		}
 		return null;
 	}
--- src/readlines/ReadLineObserverImpl.java
+++ src/readlines/ReadLineObserverImpl.java
@@ -27,6 +27,10 @@ public class ReadLineObserverImpl implements ReadLineObserver {
 			stateMachine.transitTo(new OnYesEvent());
 		} else if (NO.contains(line)) {
 			stateMachine.transitTo(new OnNoEvent());
+		} else if (SOMETIMES.contains(line)) {
+			stateMachine.transitTo(new OnSometimesEvent());
+		} else if (UNKNOWN.contains(line)) {
+			stateMachine.transitTo(new OnUnknownEvent());
 		} else if (FOOD_CATEGORY.contains(line)) {
 			stateMachine.transitTo(new OnCategoryEvent(1));
 		} else if (STATES_CATEGORY.contains(line)) {
