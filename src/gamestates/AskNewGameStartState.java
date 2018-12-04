43194c1b230d7242dfa544b55c25860a70c12a1d
Aashirya Kaushik <ak1107@wildcats.unh.edu>
2018-12-03 19:41:07 -0500

    when the system guesses the right word

---
+++ src/gamestates/AskNewGameStartState.java
@@ -0,0 +1,38 @@
+package gamestates;
+
+import java.io.IOException;
+
+import gameevents.GameEvent;
+import gameevents.OnNoEvent;
+import gameevents.OnYesEvent;
+import readlines.ReadLineManager;
+
+public class AskNewGameStartState implements GameState {
+
+	@Override
+	public void onEnter() {
+		// TODO Auto-generated method stub
+		System.out.println("Do you want to start a new game?");
+		
+		
+	}
+
+	@Override
+	public GameState transitionTo(GameEvent event) {
+		if(event instanceof OnYesEvent) {
+			return new SelectCategoryState();
+		}
+		if(event instanceof OnNoEvent) {
+			try {
+				ReadLineManager.getInstance().stopReading();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
